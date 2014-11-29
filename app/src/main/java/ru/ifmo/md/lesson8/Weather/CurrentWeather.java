06663d556f80b2ed45d8d7d60a6947968e6d3f0e
baba-beda <darya.nightmare@gmail.com>
2014-11-30 01:20:29 +0300

    Added structure of application: CurrentWeather and WeatherForecast (includes 7 Weather elements and information about the city; Added parsers for current weather and forecast (God, it's so nice to work with JSON) Added getters for data and icons

---
+++ app/src/main/java/ru/ifmo/md/lesson8/Weather/CurrentWeather.java
@@ -0,0 +1,111 @@
+package ru.ifmo.md.lesson8.Weather;
+
+
+import java.sql.Time;
+
+/**
+ * Created by daria on 29.11.14.
+ */
+public class CurrentWeather {
+    private Time sunrise, sunset;
+    private int temperature, pressure, humidity, minTemperature, maxTemperature, windSpeed, windDeg, weatherId;
+    private String name;
+    private byte[] icon;
+
+    public byte[] getIcon() {
+        return icon;
+    }
+
+    public void setIcon(byte[] icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Time getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(Time sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public Time getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(Time sunset) {
+        this.sunset = sunset;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(int windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public int getWindDeg() {
+        return windDeg;
+    }
+
+    public void setWindDeg(int windDeg) {
+        this.windDeg = windDeg;
+    }
+
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+}
